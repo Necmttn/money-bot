a39a573a44b6e0b8e2f4175ebbb91c28c789df4c
Dariia-Smyrnova <dariia.smyrnova@wesoftyou.com>
2022-12-23 14:55:41 -0600

    use telegraf

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,5 +1,32 @@
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(process.env.NEXT_TELEGRAM_TOKEN as string);
+
+// bot.hears('text', async (ctx) => {
+//   // Explicit usage
+//   await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+
+//   // Using context shortcut
+//   await ctx.reply(`Hello ${ctx.state.role}`);
+// });
+
+// bot.on('callback_query', async (ctx) => {
+//   // Explicit usage
+//   await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+
+//   // Using context shortcut
+//   await ctx.answerCbQuery();
+// });
+
 export default async (req, res) => {
   const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
+  bot.hears('text', async (ctx) => {
+    // Explicit usage
+    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+  
+    // Using context shortcut
+    await ctx.reply(`Hello ${ctx.state.role}`);
+  });
   console.log(req.body);
   
   res.status(200).send('OK');
