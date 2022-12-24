3bdea5122b6a2339d4bdc5e768fc5c2467fdcac5
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-12-24 17:10:26 -0600

    initial bot examples

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,33 +1,55 @@
-import { Telegraf } from 'telegraf';
+import { NextApiRequest, NextApiResponse } from "next";
+import { Markup, Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.NEXT_TELEGRAM_TOKEN as string);
 
-// bot.hears('text', async (ctx) => {
-//   // Explicit usage
-//   await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+/**
+ * hears method is used to listen to a specific text
+ * @param {string} text
+ * @param {Function} handler
+ */
+bot.hears("money", async (ctx) => {
+  // base on the text, we can send a message reply.
+  await ctx.telegram.sendMessage(ctx.message.chat.id, `Money is money bitch`);
+  // We can send multiple messages
+  // Even some with custo markup
+  await ctx.reply(
+    "whay",
+    Markup.inlineKeyboard([
+      Markup.button.callback("Yes", "yes"),
+      Markup.button.callback("No", "no"),
+    ])
+  );
+});
 
-//   // Using context shortcut
-//   await ctx.reply(`Hello ${ctx.state.role}`);
-// });
+bot.action("yes", async (ctx) => {
+  await ctx.answerCbQuery("You said No");
+});
 
-// bot.on('callback_query', async (ctx) => {
-//   // Explicit usage
-//   await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+bot.action("no", async (ctx) => {
+  await ctx.answerCbQuery("You said No");
+});
 
-//   // Using context shortcut
-//   await ctx.answerCbQuery();
-// });
+// This how you set up expcility commands
+bot.command("caption", (ctx) => {
+  return ctx.replyWithPhoto(
+    { url: "https://picsum.photos/200/300/?random" },
+    {
+      caption: "Caption",
+      parse_mode: "Markdown",
+      ...Markup.inlineKeyboard([
+        Markup.button.callback("Plain", "plain"),
+        Markup.button.callback("Italic", "italic"),
+      ]),
+    }
+  );
+});
 
-export default async (req, res) => {
-  const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
-  bot.hears('text', async (ctx) => {
-    // Explicit usage
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
-  
-    // Using context shortcut
-    await ctx.reply(`Hello ${ctx.state.role}`);
-  });
+bot.action(/.+/, (ctx) => {
+  return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`);
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await bot.handleUpdate(req.body, res);
   console.log(req.body);
-  
-  res.status(200).send('OK');
 };
